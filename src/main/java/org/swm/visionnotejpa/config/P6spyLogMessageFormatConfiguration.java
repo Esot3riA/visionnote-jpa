35cb8053e260d9138e2a147a4ba17a4f157a568e
Esot3riA <esot3ria@gmail.com>
2022-02-15 10:41:02 +0900

    Apply querydsl and p6spy settings

---
+++ src/main/java/org/swm/visionnotejpa/config/P6spyLogMessageFormatConfiguration.java
@@ -0,0 +1,19 @@
+package org.swm.visionnotejpa.config;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class P6spyLogMessageFormatConfiguration {
+
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatConfiguration.class.getName());      // pretty
+
+        // exclude sql pattern
+//        P6LogOptions.getActiveInstance().setFilter(true);
+//        P6LogOptions.getActiveInstance().setExclude("store");
+    }
+}
