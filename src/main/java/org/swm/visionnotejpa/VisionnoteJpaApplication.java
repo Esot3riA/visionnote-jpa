cb5458db9fe13a0a29e14f58ea980e2dc9e9613b
Esot3riA <esot3ria@gmail.com>
2022-02-15 10:14:59 +0900

    Initial commit

---
+++ src/main/java/org/swm/visionnotejpa/VisionnoteJpaApplication.java
@@ -0,0 +1,13 @@
+package org.swm.visionnotejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionnoteJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionnoteJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/swm/visionnotejpa/VisionnoteJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swm.visionnotejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionnoteJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
